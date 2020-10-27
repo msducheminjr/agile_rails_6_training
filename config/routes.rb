907c1e6cf64554b6c744cba29573cd491d8e853f
Michael Duchemin <msducheminjr@users.noreply.github.com>
2020-10-27 14:46:47 -0400

    Itration K2: Translating the Storefront
    Add translation files and modify views for Engish, Spanish and Pirate (YAR!).
    
    * Add modify YAML files for number, layouts, store, carts
    * Fix bug in routes that was preventing navigation to the products resource.

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,14 @@ Rails.application.routes.draw do
     delete 'logout' => :destroy
   end
   resources :users
+  resources :products do
+    get :who_bought, on: :member
+  end
   scope '(:locale)' do
     resources :orders
     resources :line_items
     resources :carts
     root 'store#index', as: 'store_index'
-  end
-  resources :products do
-    get :who_bought, on: :member
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
