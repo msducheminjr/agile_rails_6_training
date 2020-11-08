80f24ec247ee637de491506860ff3556a1b5ec1c
Michael Duchemin <msducheminjr@users.noreply.github.com>
2020-11-07 19:42:27 -0500

    Iteration K4: Adding a Locale Switcher
    Add a locale selector to the application layout that allows the user to
    choose from available locales and switches the locale based on the
    locale selected. Added locale switcher to store catalog system test.
    Added javascript pack and tag for locale switcher.
    
    NOTE: The implementation of this should probably be reworked. The locale
    switcher always redirects to the store index path, which would not be
    the best user experience if you are on any other page of the app.

--- test/system/store_catalog_test.rb
+++ test/system/store_catalog_test.rb
@@ -3,12 +3,23 @@ require "application_system_test_case"
 class StoreCatalogTest < ApplicationSystemTestCase
   setup do
     @product = products(:ruby)
+    I18n.locale = I18n.default_locale
   end
 
-  test "visiting the index" do
+  teardown do
+    I18n.locale = I18n.default_locale
+  end
+
+  test "visiting the index and switching locales" do
     visit '/'
     assert_selector "h1", text: "Your Pragmatic Catalog"
     assert_selector "ul.catalog li h2", text: @product.title
+    select 'Pirate', from: "set_locale"
+    assert_selector "h1", text: "Yer Pragmatic Catalog"
+    select 'Español', from: "set_locale"
+    assert_selector "h1", text: "Su Catálogo de Pragmatic"
+    select 'English', from: "set_locale"
+    assert_selector "h1", text: "Your Pragmatic Catalog"
   end
 
   test "adding an item to the cart" do
